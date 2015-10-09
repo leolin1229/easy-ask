10b4ff093d6df99880814b4dc09befeb7d22b369
leolin <787659058@qq.com>
2015-10-09 20:19:34 +0800

    add 'Promise' for request chain method

--- index.js
+++ index.js
@@ -1,4 +1,96 @@
-var Request = require('./ask'),
-	r = new Request();
+var http = require('http'),
+	qs = require('querystring');
 
-module.exports = r;
+function Ask(host, path, options, body) {
+	options = options || {};
+
+	options.host = host || 'localhost';
+	options.path = path || '/';
+	options.port = options.port || 80;
+	options.timeout = options.timeout || 15000;
+	options.method = options.method.toUpperCase() || 'GET';
+
+	return new Promise(function(resolve, reject) {
+		ask(resolve, reject, options, body);
+	});
+}
+
+function ask(resolve, reject, options, body) {
+	var data = null;
+
+	options.headers = options.headers || {};
+	options.expect && (options.headers['Expect'] = '100-Continue');
+	options.query && (options.path += '?' + qs.stringify(options.query));
+
+	if (body) {
+		if (typeof body == 'object') {
+
+			// maybe catch exception
+			data = JSON.stringify(body);
+			data = Buffer(data);
+
+			options.headers['Content-Type'] = 'application/json';
+			(!options.headers['Accept']) && (options.headers['Accept'] = 'application/json');
+			// maybe catch exception
+		} else {
+			data = Buffer(data + '');
+			options.headers['Content-Type'] = 'text/plain';
+		}
+
+		options.headers['Content-Length'] = data.length;
+	}
+
+	var req = http.request(options);
+
+	if (options.timeout && options.timeout > 0) {
+		req.on('socket', function(socket) {
+			socket.setTimeout(options.timeout);
+			socket.on('timeout', function() {
+				req.abort();
+			});
+		});
+	}
+
+	req.on('error', function(err) {
+		reject(err);
+	});
+
+	req.on('abort', function() {
+		reject('Error: The request is aborted!')
+	});
+
+	req.on('response', function(res) {
+		res.setEncoding('utf8');
+
+		res.data = '';
+		res.on('data', function(d) {
+			res.data += d.toString('utf8');
+		});
+
+		res.on('end', function() {
+			try {
+				res.body = JSON.parse(res.data);
+			} catch(e) {
+				console.error('Error: Invalid json data!');
+				return ;
+			};
+			resolve(res.data);
+		});
+	});
+
+	if (data) {
+		options.expect && req.on('continue', function() {
+			req.write(data);
+			req.end();
+		});
+
+		(!options.expect) && (function() {
+			req.write(data);
+			req.end();
+		})();
+	} else {
+		req.end();
+	}
+};
+
+module.exports = Ask;
--- tests/test.js
+++ tests/test.js
@@ -1,15 +1,70 @@
 var ask = require('../index');
 
-ask.get('busi.vip.kankan.com', '/userinfo/getUserInfo', {
-	query: {
-		respType: 'json'
-	},
-	headers: {
-		'Cookie': {
-			userid: '123455'
-		}
-	}
-}, function(err, res) {
-	// console.log(err + '!!');
-	console.log(res.data);
-});
+ask('busi.vip.kankan.com', '/userinfo/getUserInfo', {
+		method: 'GET',
+		query: {
+			respType: 'json'
+		},
+		headers: {
+			'Cookie': {
+				userid: '123455'
+			}
+		},
+		timeout: 1000
+	})
+	.then(function(data) {
+		console.log(data);
+		return ask('svr.act.vip.kankan.com', '/giftService/saveUserAddress', {
+			method: 'POST',
+			query: {
+				respType: 'json'
+			},
+			timeout: 1000
+		}, {
+			address: 'fafasfaa'
+		});
+	})
+	.then(function(data) {
+		console.log(data);
+		return Promise.resolve('Bingo');
+	})
+	.then(function(data) {
+		console.log(data);
+	});
+// var p = new Promise(function(resolve, reject) {
+// 	ask.get('busi.vip.kankan.com', '/userinfo/getUserInfo', {
+// 		query: {
+// 			respType: 'json'
+// 		},
+// 		headers: {
+// 			'Cookie': {
+// 				userid: '123455'
+// 			}
+// 		},
+// 		timeout: 1000
+// 	}, function(err, res) {
+// 		err && console.log(err);
+// 		res && res.data && (res.data = JSON.parse(res.data));
+// 		res && resolve(res.data);
+// 	});
+// });
+
+// p.then(function(val){
+// 	// console.log(val);
+// 	ask.post('svr.act.vip.kankan.com', '/giftService/saveUserAddress', {
+// 		query: {
+// 			respType: 'json'
+// 		},
+// 		timeout: 1000
+// 	},{
+// 		address: 'fasdfadsfa'
+// 	}, function(err, res) {
+// 		err && console.log(err);
+// 		res && res.data && (res.data = JSON.parse(res.data));
+// 		res && console.log(res.data);
+// 		return res.data;
+// 	});
+// }, function() {})
+// .then(function(val) {
+// 	console.log(val);
+// }, function() {});
